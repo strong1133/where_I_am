dbabab35850e95753e8e534acd817d6bbadb1782
strong1133 <strong1133@naver.com>
2022-05-30 09:15:09 +0900

    msg test code

---
+++ backend/test/msg.spec.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const should = require("should");
+
+const app = require("../app");
+
+describe("GET /user 는", () => {
+    describe("성공시", () => {
+        it("200을 반환한다.", (done) => {
+            request(app).get('/users').expect(200).end(done);
+        });
+    });
+});
+
+describe("Post /msg/push 는", () => {
+    describe("성공시 ", () => {
+        let body;
+        before((done) => {
+            request(app)
+                .post("/msg/push")
+                .send({
+                    deviceToken: "testToken1",
+                    userId: "tjrwls455",
+                })
+                .expect(200)
+                .end((err, res) => {
+                    body = res.body;
+                });
+        });
+        it("요청 된 userId와 push 성공유무를 반환한다.", (done) => {
+            body.should.have.property("flag").describe(true);
+        });
+    });
+    describe("실패시 ", () => {
+        it("deviceToken 누락시 400을 응답한다.", (done) => {
+            request(app).post("/msg/push").send({ deviceToken: "" }).expect(400).end(done);
+        });
+
+        it("push 실패시 500을 응답한다.", (done) => {
+            request(app).post("/msg/push").send({ deviceToken: "testToken1", userId: "tjrwls455" }).expect(500).end(done);
+        });
+    });
+});
