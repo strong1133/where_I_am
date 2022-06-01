03e16970859f1df4848695b0dac3fb3eaa83f0fa
strong1133 <strong1133@naver.com>
2022-06-01 15:38:49 +0900

    도메인 노출 제거

--- backend/docker/docker_build.sh
+++ backend/docker/docker_build.sh
@@ -3,12 +3,12 @@ read VER
 
 echo "NODE MODULES REMOVE"
 
-rm -rf ../node_modules
+rm -rf  ../node_modules
 rm ../package-lock.json
 
 echo ":::: DOCER BUILD START / ${VER} ::::"
 # # 
-# sudo docker build -t docker.strongsub.com/strong_wiam_back:v_${VER}  .
-docker buildx build --platform linux/arm/v7 -t docker.strongsub.com/strong_wiam_back:v_${VER} --push .
+# sudo docker build -t docker.<domain>.com/strong_wiam_back:v_${VER}  .
+docker buildx build --platform linux/arm/v7 -t docker.<domain>.com/strong_wiam_back:v_${VER} --push .
 
 echo ":::: DOCER BUILD  END  ::::"
--- backend/docker/docker_cmd.md
+++ backend/docker/docker_cmd.md
@@ -3,8 +3,8 @@
 ## BackEnd 
 
 1. Docker Build
-- sudo docker build -t docker.strongsub.com/strong_wiam_back:${VER}
-- docker buildx build --platform linux/arm/v7 -t docker.strongsub.com/strong_wiam_back:${VER} --push
+- sudo docker build -t docker.<domain>.com/strong_wiam_back:${VER}
+- docker buildx build --platform linux/arm/v7 -t docker.<domain>.com/strong_wiam_back:${VER} --push
 
 2. Stack Deploy 
  - VER=v_0.0.2 docker stack deploy -c /home/strong1133/docker/strong_wiam_back/docker_swarm.yml  strongserver
